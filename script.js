67519d7875d583311384b53ebefacbefbfc8db4e
Katrina Moody <kmfoster212@gmail.com>
2024-02-19 17:04:19 -0500

    Code for Guess My Number! game

---
+++ script.js
@@ -0,0 +1,66 @@
+'use strict';
+/*
+console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.message').textContent = '🎉 Correct Number!';
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+
+document.querySelector('.guess').value = 23;
+*/
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highscore = 0;
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+
+  //When there is no input
+  if (!guess) {
+    //document.querySelector('.message').textContent = '⛔ Make a guess!';
+    displayMessage('⛔ Make a guess!');
+
+    //When player wins
+  } else if (guess === secretNumber) {
+    //document.querySelector('.message').textContent = '🎉 Correct Number!';
+    displayMessage('🎉 Correct Number!');
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+    document.querySelector('.number').textContent = secretNumber;
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
+
+    //When guess is wrong
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      //document.querySelector('.message').textContent =
+      displayMessage(guess > secretNumber ? '❌ Too High!' : '❌ Too Low!');
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      //document.querySelector('.message').textContent = '💀 You lost the game';
+      displayMessage('💀 You lost the game');
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
+
+// CLICKING THE "AGAIN" BUTTON
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  displayMessage('Start guessing...');
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+  document.querySelector('.guess').value = '';
+});
